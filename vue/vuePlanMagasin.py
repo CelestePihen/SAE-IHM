2726bad24cfe07f028034b5e5584cf66813d3210
MatisseDrin <matissedrin123@gmail.com>
2025-06-11 13:58:17 +0200

    Update vuePlanMagasin.py

--- vue/vuePlanMagasin.py
+++ vue/vuePlanMagasin.py
@@ -6,7 +6,6 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import Qt, QRectF, pyqtSignal, QPointF
 from PyQt6.QtGui import QPixmap, QPen, QBrush, QColor, QMouseEvent
-import heapq
 
 class VuePlanMagasin(QGraphicsView):
     """Vue pour afficher et manipuler le plan du magasin"""
